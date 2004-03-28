601cd131f01ae1cba24ccf3df6c733ba8fabf90f
patachou <patachou>
2004-03-28 23:02:48 +0000

    nouvelle classe d'implementation

--- test/src/fshare/client/ClientImpl.java
+++ test/src/fshare/client/ClientImpl.java
@@ -14,73 +14,65 @@ import java.util.logging.Level;
 
 import fshare.remote.RemoteClient;
 import fshare.remote.RemoteServeur;
-import fshare.gui.Main;
+import java.util.Map;
+import java.util.Collections;
+import java.util.HashMap;
+import fshare.commun.AttributFichierClient;
 
 public class ClientImpl extends java.rmi.server.UnicastRemoteObject implements RemoteClient
 {
 
-  private static Logger logger =
-        Logger.getLogger("fshare.client");
+  /**
+   * Represente la liste de fichier que possède le client.
+   * La clé est l'identifiant du fichier et les valeurs sont des objets de type
+   * AttributFichierClient.
+   */
+  private Map listeFichier;
 
-  private Main appli             = null;
-  private String nomClient       = null;
-  private RemoteServeur fServeur = null;
-
-  public ClientImpl(String serverURL, String nomClient) throws java.rmi.RemoteException
+  public ClientImpl () throws java.rmi.RemoteException
   {
-    try {
-      logger.info ("Connexion au serveur...");
-      fServeur = (RemoteServeur) java.rmi.Naming.lookup(serverURL);
-      logger.info ("Connexion REUSSIE");
-    } catch (java.net.MalformedURLException e) {
-      throw new java.rmi.RemoteException("The URL of the server if not well formed");
-    } catch (java.rmi.NotBoundException e) {
-      throw new java.rmi.RemoteException("The server was not found at the given URL");
-    }
-    this.nomClient = nomClient;
+    /* Création de la hasmap synchronizée */
+    listeFichier = Collections.synchronizedMap (new HashMap ());
   }
 
-
-  public static void main (String [] args) throws Exception
+  /**
+   * Ajoute le fichier à la liste du client.
+   * @param idFichier l'identifiant du fichier.
+   * @param atc les attributs du fichier pour le client.
+   */
+  public void ajouterFichier (String idFichier, AttributFichierClient atc)
   {
-    //Logger.getLogger("fshare.client").setLevel(Level.WARNING);
-System.out.println ("Entrer dans le main");
-
-    if (args.length < 2) {
-      System.out.println("You must give the URL of the chat server and the name of the client as arguments");
-      System.out.println(" ex : ClientImpl rmi://clio.unice.fr/Server bob");
-      System.exit(1);
-    }
-System.out.println ("Configuration security manager");
-
-    // Create and install a security manager
-    if (System.getSecurityManager() == null) {
-      System.setSecurityManager(new java.rmi.RMISecurityManager());
-    }
-    System.out.println ("Creation client");
-    ClientImpl client = new ClientImpl(args[0], args[1]);
-    System.out.println ("Mise en route client");
-    client.startClient();
-    System.out.println ("sortie le main");
-
+    listeFichier.put(idFichier, atc);
   }
 
-  void startClient ()
+  /**
+   * Retire le fichier à la liste du client.
+   * @param idFichier String
+   */
+  public void retirerFichier (String idFichier)
   {
-    try{
-    fServeur.ajouterFichier (new fshare.commun.Fichier ("fshare.html", 500, 1), this,
-                             new fshare.commun.AttributFichierClient (new java.util.ArrayList (),
-                                                        10, true));
-    }
-    catch (java.rmi.RemoteException Ex)
-    {
-      Ex.printStackTrace ();
-    }
-    appli = new Main ();
+    listeFichier.remove(idFichier);
   }
 
   public void telechargerFichier(String id, int partie)
   {
 System.out.println ("Telechargement de : " + id + ", partie : " + partie);
   }
-}
+
+  /**
+   * Fonction de débug.
+   */
+  public void afficheListeFichierClient ()
+  {
+    Object [] c = listeFichier.values().toArray();
+    for (int i = 0; i < c.length; ++i)
+    {
+      AttributFichierClient afc = (AttributFichierClient) c [i];
+      System.out.println ("Nom Fichier : " + afc.getNomFichierAbsolu() +
+                          ", du client : " + afc.getNomClient() +
+                          ", contient '" + afc.getNbPartieTotale() + "' au total" +
+                          ", et il est " + ((afc.fichierComplet()) ? "complet" : "incomplet") +
+                          ", et sa date est : " + afc.getDateDerModif());
+    }
+  }
+} // Classe ClientImpl
