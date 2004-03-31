f499a2cf397143ce5d9d3e0a96e8ba8b3d50f4ad
daimon <daimon>
2004-03-31 14:58:31 +0000

    Création de la liste de partage

--- v3/src/fshare/gui/Main.java
+++ v3/src/fshare/gui/Main.java
@@ -1,33 +1,65 @@
+
+/* Version 1.1*/
 package fshare.gui;
 
 import javax.swing.*;
+
 import java.awt.Dimension;
 import java.util.Vector;
+
+
+
 import java.awt.GridLayout;
+
+
 import java.awt.*;
+
+
+
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
+
+
+
+
+
 import java.awt.GridBagConstraints;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import fshare.remote.RemoteClient;
+import java.io.File;
+import fshare.commun.Fichier;
+import java.util.ArrayList;
 
 public class Main {
   JFrame frame = new JFrame("fshare");
+  //panels correspondant aux onglets
+  private RemoteClient controleur;
+
   JPanel panelD = new JPanel();
   JPanel panelU = new JPanel();
-  JPanel panelS = new JPanel();
+  JPanel panelP = new JPanel();
   JPanel panelC = new JPanel();
   JPanel panelR = new JPanel();
   JPanel panelA = new JPanel();
 
   JTextField fieldR = new JTextField("regexp...",30);
+  JTextField fieldP = new JTextField("/",30);
+  String partagePath;
+  ArrayList fichierPartage = new ArrayList();
+
   JButton boutR = new JButton("Télécharger");
 
   JTabbedPane onglets = new JTabbedPane();
-  JTable download;
-  JTable upload;
-  JTable recherche;
+  JTable downloadTable;
+  JTable uploadTable;
+  JTable rechercheTable;
+  JTable partageTable;
   JScrollPane scrollD;
-  JScrollPane scrollU = new JScrollPane(upload);
+  JScrollPane scrollU = new JScrollPane(uploadTable);
   JScrollPane scrollR;
+  JScrollPane scrollP;
+
   /*
   JMenuBar menu = new JMenuBar();
   JMenu menuAction = new JMenu("Action");
@@ -36,6 +68,13 @@ public class Main {
 */
   String[] columnsD = {
       "Nom du fichier", "ID", "Nombre de parties", "Parties restantes", "Date"};
+  String[] columnsU = {
+      "Nom du fichier", "ID", "Nombre de parties", "Parties envoyées", "Date"};
+  String[] columnsR = {
+      "Nom du fichier", "ID", "Nombre de parties", "Nombre de sources", "Date"};
+  String[] columnsP = {
+      "Nom du fichier", "ID", "Nombre de parties", "Parties reçues", "Date"};
+
   Object[][] dataD = {
       {
       "emule.exe", "EFZFEGG435R34TG436T", new Integer(5), new Integer(2),
@@ -43,25 +82,25 @@ public class Main {
   };
 
   private void constructionD() {
-    download = new JTable(dataD, columnsD);
-    scrollD = new JScrollPane(download);
+    downloadTable = new JTable(dataD, columnsD);
+    scrollD = new JScrollPane(downloadTable);
     panelD.setLayout(new BorderLayout());
     panelD.add(scrollD, BorderLayout.CENTER);
   }
 
   private void constructionU() {
-    upload = new JTable(dataD, columnsD);
-    scrollU = new JScrollPane(upload);
+    uploadTable = new JTable(dataD, columnsU);
+    scrollU = new JScrollPane(uploadTable);
     panelU.setLayout(new BorderLayout());
     panelU.add(scrollU, BorderLayout.CENTER);
   }
 
   private void constructionR(){
     SpringLayout layout = new SpringLayout();
-      BorderLayout bol = new BorderLayout();
+    BorderLayout bol = new BorderLayout();
     JLabel r = new JLabel("Rechercher : ");
-    recherche = new JTable(dataD, columnsD);
-    scrollR = new JScrollPane(recherche);
+    rechercheTable = new JTable(dataD, columnsR);
+    scrollR = new JScrollPane(rechercheTable);
     JPanel rechPane = new JPanel();
     rechPane.setLayout(layout);
       panelR.setLayout(bol);
@@ -82,8 +121,97 @@ public class Main {
 panelR.add(scrollR, BorderLayout.CENTER);
 }
 
-  public Main() {
+private void constructionP(){
+  SpringLayout layout = new SpringLayout();
+  BorderLayout bol = new BorderLayout();
+  JButton boutAjout = new JButton("Ajouter un fichier");
+  JButton boutRetire = new JButton("Retirer un fichier");
+  JButton boutBrowse = new JButton("...");
+  JLabel partLabel = new JLabel("Rép. par défaut ");
+
+  partageTable = new JTable(dataD, columnsP);
+  scrollP = new JScrollPane(partageTable);
+  JPanel partPane = new JPanel();
+  partPane.setLayout(layout);
+    panelP.setLayout(bol);
+
+  partPane.setPreferredSize(new Dimension(10, 35));
+  layout.putConstraint(SpringLayout.WEST, boutAjout, 5, SpringLayout.WEST, partPane);
+  layout.putConstraint(SpringLayout.NORTH, boutAjout, 5, SpringLayout.NORTH, partPane);
+  layout.putConstraint(SpringLayout.WEST, boutRetire, 5, SpringLayout.EAST, boutAjout);
+  layout.putConstraint(SpringLayout.NORTH, boutRetire, 5, SpringLayout.NORTH, partPane);
+  layout.putConstraint(SpringLayout.WEST, partLabel, 10, SpringLayout.EAST, boutRetire);
+  layout.putConstraint(SpringLayout.NORTH, partLabel, 10, SpringLayout.NORTH, partPane);
+  layout.putConstraint(SpringLayout.WEST, fieldP, 5, SpringLayout.EAST, partLabel);
+  layout.putConstraint(SpringLayout.NORTH, fieldP, 7, SpringLayout.NORTH, partPane);
+  layout.putConstraint(SpringLayout.WEST, boutBrowse, 5, SpringLayout.EAST, fieldP);
+  layout.putConstraint(SpringLayout.NORTH, boutBrowse, 5, SpringLayout.NORTH, partPane);
+
+  boutBrowse.addMouseListener(new MouseListener(){
+      public void mouseClicked(MouseEvent e) {
+                JFileChooser repChooser = new JFileChooser();
+        repChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int returnVal = repChooser.showOpenDialog(frame);
+        if(returnVal == JFileChooser.APPROVE_OPTION){
+          partagePath = repChooser.getSelectedFile().getPath();
+          fieldP.setText(partagePath);
+          majListePartage();
+        }
+      }
+      public void mouseEntered(MouseEvent e) {
+      }
+      public void mouseExited(MouseEvent e) {
+      }
+      public void mousePressed(MouseEvent e) {
+      }
+      public void mouseReleased(MouseEvent e) {
+      }
+
+    });
+
+  partPane.add(boutAjout);
+  partPane.add(boutRetire);
+  partPane.add(partLabel);
+  partPane.add(fieldP);
+  partPane.add(boutBrowse);
+  panelP.add(partPane, BorderLayout.NORTH);
+  panelP.add(scrollP, BorderLayout.CENTER);
+}
+
+public void majListePartage(){
+  ajoutRecursif(new File(partagePath));
+  //partageTable.getModel().add
+//  partageTable.getModel().setValueAt(fichierPartage.get(0),1,1);
+
+}
+
+  public Main(RemoteClient controleur) {
+    this.controleur = controleur;
     assemble();
+  }
+
+
+
+  private void ajoutRecursif(File d) {
+
+    if (d.isDirectory()) {
+      File[] sub = d.listFiles();
+      for (int i = 0; i < sub.length; i++) {
+        if (sub[i].isDirectory()) {
+          ajoutRecursif(sub[i]);
+          continue;
+        }
+        try{
+          fichierPartage.add(new Fichier(sub[i].toString()));
+        }
+        catch (OutOfMemoryError e){
+          System.out.println("ERROR "+sub[i].toString());
+        }
+      }
+    }
+    if (d.isFile()) {
+      fichierPartage.add(new Fichier(d.toString()));
+    }
   }
 
   public void assemble() {
@@ -94,7 +222,7 @@ panelR.add(scrollR, BorderLayout.CENTER);
                    "Gestion des envois");
     onglets.addTab("Recherche", new ImageIcon("images/rech.gif"), panelR,
                    "Recherche de fichiers");
-    onglets.addTab("Partage", new ImageIcon("images/share.gif"), panelS,
+    onglets.addTab("Partage", new ImageIcon("images/share.gif"), panelP,
                    "Gestion des fichiers partagés");
     onglets.addTab("Configuration", new ImageIcon("images/config.gif"), panelC,
                    "Configuration de l'application");
@@ -106,6 +234,7 @@ panelR.add(scrollR, BorderLayout.CENTER);
     constructionD();
     constructionU();
     constructionR();
+    constructionP();
     /*
         menu.add(menuAction);
         menuAction.add(itemConfig);
@@ -118,7 +247,7 @@ panelR.add(scrollR, BorderLayout.CENTER);
   }
 
   public static void main(String[] arg) {
-    Main appli = new Main();
+    Main appli = new Main(null);
 
   }
 
