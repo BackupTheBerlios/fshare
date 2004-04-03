45174d8fd10c1e9aff2be846597b7d4e369ed91b
daimon <daimon>
2004-04-03 12:29:22 +0000

    modifs assez nombreuses pour l'interfacage avec la gui

--- v3/src/fshare/client/Client.java
+++ v3/src/fshare/client/Client.java
@@ -24,21 +24,76 @@ import java.util.*;
 public class Client {
 
   public static final String DEFAULT_SHARE_REP = "partage";
-  public static final String FIC_PROPRIETE     = "setting.props";
-
+  public static final String FIC_PROPRIETE = "setting.props";
 
   private static Logger logger =
       Logger.getLogger("fshare.client");
 
-  private Main          appli             = null;
-  private String        nomClient         = null;
-  private RemoteServeur fServeur          = null;
-  private ClientImpl    client            = null;
-  private String        repertoirePartage = null;
-  private ArrayList     fichiersPartage   = new ArrayList();
+  private Main appli = null;
+  private String nomClient = null;
+  private RemoteServeur fServeur = null;
+  private ClientImpl client = null;
+  private String repertoirePartage = null;
+  private String urlServer = null;
+  private ArrayList fichiersPartage = new ArrayList();
 
-  public Client(String serverURL, String nomClient) throws java.rmi.RemoteException
-  {
+  public Client(String serverURL, String nomClient) throws java.rmi.
+      RemoteException {
+    if (serverURL == null || nomClient == null) {
+      serverURL = Propriete.getPropriete(FIC_PROPRIETE, "urlServer");
+      nomClient = Propriete.getPropriete(FIC_PROPRIETE, "nickName");
+    }
+    urlServer = serverURL;
+    this.nomClient = nomClient;
+
+    if (serverURL == null || nomClient == null) {
+      client = new ClientImpl();
+      prepareInfoFichierClient();
+      appli = new Main(this);
+    }
+    else {
+      try {
+
+        /* Récupere l'objet serveur distant */
+        logger.info("Connexion au serveur...");
+        fServeur = (RemoteServeur) java.rmi.Naming.lookup(serverURL);
+        logger.info("Connexion REUSSIE");
+      }
+      catch (java.net.MalformedURLException e) {
+        throw new java.rmi.RemoteException(
+            "The URL of the server if not well formed");
+      }
+      catch (java.rmi.NotBoundException e) {
+        throw new java.rmi.RemoteException(
+            "The server was not found at the given URL");
+      }
+      client = new ClientImpl();
+      this.nomClient = nomClient;
+      prepareInfoFichierClient();
+      //client.afficheListeFichierClient();
+      appli = new Main(this);
+
+    }
+  }
+
+  public String getServerName() {
+    return urlServer;
+  }
+
+  public String getNickName() {
+    return nomClient;
+  }
+
+  public boolean isConnected(){
+    return (fServeur != null);
+  }
+
+  public void deconnect(){
+    fServeur = null;
+  }
+
+  public void connectToServer(String serverURL, String nomClient) throws java.
+      rmi.RemoteException {
     try {
 
       /* Récupere l'objet serveur distant */
@@ -57,17 +112,16 @@ public class Client {
     this.nomClient = nomClient;
     client = new ClientImpl();
 
-    /* On récupere le fichier de partage */
-    repertoirePartage = Propriete.getPropriete(FIC_PROPRIETE, "shareDir");
-    if (null == repertoirePartage)
-      repertoirePartage = DEFAULT_SHARE_REP;
-    logger.info("Votre repertoire de partage est : " + repertoirePartage);
+    setRepertoirePartage(repertoirePartage);
+
+    Propriete.setPropriete(FIC_PROPRIETE, "urlServer", serverURL);
+    Propriete.setPropriete(FIC_PROPRIETE, "nickName", nomClient);
 
   }
 
-  public String[] getFichiers(){
+  public String[] getFichiers() {
     return client.getFichierClient();
-}
+  }
 
   public static void main(String[] args) throws Exception {
     Logger.getLogger("fshare.client").setLevel(Level.ALL);
@@ -84,39 +138,61 @@ public class Client {
     if (System.getSecurityManager() == null) {
       System.setSecurityManager(new java.rmi.RMISecurityManager());
     }
-    System.out.println("Creation client");
-    Client fclient = new Client(args [0], args [1]);
-    System.out.println("Mise en route client");
+    System.out.println("Création du client");
 
-    fclient.startClient();
-    System.out.println("sortie le main");
+    //Client fclient = new Client(args [0], args [1]);
+    Client fclient = new Client(null, null);
+
+    System.out.println("Démarrage du client");
+    //fclient.startClient();
 
   }
 
-  private void startClient ()
-  {
-    prepareInfoFichierClient ();
-client.afficheListeFichierClient();
-    appli = new Main (this);
+  private void startClient() {
+    try {
+      client = new ClientImpl();
+    }
+    catch (RemoteException ex) {
+      System.out.println(ex.getMessage());
+    }
+
+    prepareInfoFichierClient();
+//client.afficheListeFichierClient();
+    appli = new Main(this);
   }
 
   /**
    * Prépare les informations necessaire au bon fonctionnement du client
    */
-  private void prepareInfoFichierClient ()
-  {
+  private void prepareInfoFichierClient() {
+  /* On récupere le fichier de partage */
+    //repertoirePartage = Propriete.getPropriete(FIC_PROPRIETE, "shareDir");
+
+    if (null == repertoirePartage) {
+      repertoirePartage = DEFAULT_SHARE_REP;
+    }
+    logger.info("Votre repertoire de partage est : " + repertoirePartage);
+
     /* Ajout des fichiers du repertoire de partage */
-    lectureFichierDansRepertoire (new File (repertoirePartage));
+    lectureFichierDansRepertoire(new File(repertoirePartage));
 
     /* Ajout des fichiers en cours de téléchargement */
   }
 
-  public void setRepertoirePartage(String rep){
+  public void setRepertoirePartage(String rep) {
     repertoirePartage = rep;
-    prepareInfoFichierClient();
+    if (fServeur != null) {
+      prepareInfoFichierClient();
+
+    }
+    Propriete.setPropriete(FIC_PROPRIETE, "shareDir", rep);
   }
 
-  public Fichier[] rechercherFichier(String regexp){
+  public String getRepertoirePartage() {
+    return repertoirePartage;
+  }
+
+  public Fichier[] rechercherFichier(String regexp) {
 
     try {
       return fServeur.rechercherFichier(regexp);
@@ -127,43 +203,44 @@ client.afficheListeFichierClient();
     return null;
   }
 
-
   /**
    * Récupere tous les fichiers dans le repertoire de partage (et sous repértoires)
    *  et les ajoute à
    * la liste du client avec les bonnes informations.
    * @param rep le répertoire racine.
    */
-  private void lectureFichierDansRepertoire (File rep)
-  {
-    if (rep.isDirectory ())
-    {
-      File[] list = rep.listFiles ();
-      for (int i = 0; i < list.length; i++)
-      {
-        if (list[i].isDirectory()) /* Appel récursif sur les sous-répertoires */
-          lectureFichierDansRepertoire (list[i]);
-        else /* C'est un fichier */
-        {
-            /* Création des informations sur le fichier */
-            Fichier f = new Fichier (list[i].toString(), list[i].length(), Fichier.getTypeFichier(list[i]));
-            AttributFichierClient atc = new AttributFichierClient (list[i].getAbsolutePath(),
-                                                                   null, 1, true, nomClient);
-            /* Enregistrement du fichier chez le client */
-            client.ajouterFichier(f.getIdFichier(), atc);
-            fichiersPartage.add(f);
-            /* Enregistrement au niveau du serveur */
-            try
-            {
-              fServeur.ajouterFichier (f, client, atc);
-
+  private void lectureFichierDansRepertoire(File rep) {
+    if (rep.isDirectory()) {
+      File[] list = rep.listFiles();
+      for (int i = 0; i < list.length; i++) {
+        if (list[i].isDirectory())
+            /* Appel récursif sur les sous-répertoires */
+             {
+          lectureFichierDansRepertoire(list[i]);
+        }
+        else
+        /* C'est un fichier */ {
+          /* Création des informations sur le fichier */
+          Fichier f = new Fichier(list[i].toString(), list[i].length(),
+                                  Fichier.getTypeFichier(list[i]));
+          AttributFichierClient atc = new AttributFichierClient(list[i].
+              getAbsolutePath(),
+              null, 1, true, nomClient);
+          /* Enregistrement du fichier chez le client */
+          client.ajouterFichier(f.getIdFichier(), atc);
+          fichiersPartage.add(f);
+          /* Enregistrement au niveau du serveur */
+          try {
+            if (fServeur != null) {
+              fServeur.ajouterFichier(f, client, atc);
 
             }
-            catch (java.rmi.RemoteException e)
-            {
-              logger.severe("Impossible d'ajouter le fichier sur le serveur");
-              e.printStackTrace();
-            }
+
+          }
+          catch (java.rmi.RemoteException e) {
+            logger.severe("Impossible d'ajouter le fichier sur le serveur");
+            e.printStackTrace();
+          }
         }
       }
     }
